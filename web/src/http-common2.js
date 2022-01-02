3441f5b020edfd796ea38e7aabc869b8e8bf3c39
kcs1580 <38133871+kcs1580@users.noreply.github.com>
2022-01-02 15:49:05 +0900

    Delete web directory

--- web/src/http-common2.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  //baseURL: "http://192.168.28.129:8397/api/auth",
-  baseURL: "http://localhost:8397/api/auth",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
